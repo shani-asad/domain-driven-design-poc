045df9bdfde85aebaf831f397f6fce6f9b68badf
a-shani <a.shani@samsung.com>
2021-04-08 11:47:49 +0700

    clear comments

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/clinics');
 });
-
-//Route::get('/clinics', function() {
-//    $select = DB::select('select * from clinics');
-//    return view('clinics.index')->with('name',$select);
-//})->name('clinics');
-//
-//Route::get('/registerClinics', [RegisterClinicController::class, 'index'])->name('registerClinic');
-//Route::post('/registerClinics', [RegisterClinicController::class, 'store']);
